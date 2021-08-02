977ce169a0efcbe606665c28abaf232e862427ec
PlengNakdee <plengwn@gmail.com>
2021-08-02 06:33:28 +0000

    initial setup

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Movie App</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="" />
+      </Head>
+
+      <div>
+        <p>Hello</p>
+      </div>
+    </div>
+  )
+}
