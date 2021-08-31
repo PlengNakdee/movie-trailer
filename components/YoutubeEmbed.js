5bf0d8dae90de496f4fda66ae21c5fcad25d345b
PlengNakdee <plengwn@gmail.com>
2021-08-31 08:01:23 +0000

    modify youtube embed

--- components/YoutubeEmbed.js
+++ components/YoutubeEmbed.js
@@ -1,11 +1,9 @@
-import React from "react";
-import PropTypes from "prop-types";
+// import React from "react";
+// import PropTypes from "prop-types";
 
 const YoutubeEmbed = ({ embedId }) => (
-  <div className="absolute sm:w-full sm:h-full">
-    <iframe
-      width="100%"
-      height="100%"
+  <div className="">
+    <iframe className="absolute inset-0 sm:w-full sm:h-full"
       src={`https://www.youtube.com/embed/${embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -15,8 +13,8 @@ const YoutubeEmbed = ({ embedId }) => (
   </div>
 );
 
-YoutubeEmbed.propTypes = {
-  embedId: PropTypes.string.isRequired
-};
+// YoutubeEmbed.propTypes = {
+//   embedId: PropTypes.string.isRequired
+// };
 
 export default YoutubeEmbed;
